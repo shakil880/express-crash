3ea71f50322ac77989132ca342756e09fd96aa2e
Shakil Quraishi <shakil880@gmail.com>
2025-11-04 02:02:52 +0600

    2nd commit

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("Hello World!")
 })
 
-app.listen(port, () => {
+
+app.get('/about', (req, res) => {
+  res.send("About Page")
+})
+
+
+
+
+app.listen(port, () => {  
   console.log(`Example app listening on port ${port}`)
 })
