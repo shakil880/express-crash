20f918d0879769bee8d808c3a0d9d0d6efc9ff91
Shakil Quraishi <shakil880@gmail.com>
2025-11-05 13:56:10 +0600

    3rd commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
-
-
-app.get('/about', (req, res) => {
-  res.send("About Page")
-})
-
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 
